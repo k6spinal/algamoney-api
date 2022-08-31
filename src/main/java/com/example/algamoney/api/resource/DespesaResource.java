510a80a6ee8d12001b7294002d48410bb9457e2a
k6spinal <k6spinal@gmail.com>
2022-08-30 23:34:42 -0300

    alterando metodo post despesa

--- src/main/java/com/example/algamoney/api/resource/DespesaResource.java
+++ src/main/java/com/example/algamoney/api/resource/DespesaResource.java
@@ -4,21 +4,17 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.example.algamoney.api.dto.CategoriaDTO;
 import com.example.algamoney.api.dto.DespesaDTO;
-import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.model.Despesa;
 import com.example.algamoney.api.repository.DespesaRepository;
 import com.example.algamoney.api.service.DespesaService;
@@ -38,14 +34,20 @@ public class DespesaResource {
 //		Categoria categoriaSalva = categoriaService.findById(id);
 //		BeanUtils.copyProperties(objDto, categoriaSalva, "id");
 //		categoriaRepository.save(categoriaSalva);
-//		return ResponseEntity.ok(categoriaSalva);
+//		return ResponseEntity.ok(categuoriaSalva);
 //	}	
 	
 	@PostMapping
 	public ResponseEntity<Despesa> create(@RequestBody Despesa obj) {
 		obj = despesaService.create(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId_despesa()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getCategoria().getId_categoriadespesa()).toUri();
 		return ResponseEntity.created(uri).build();
+	}	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Despesa> findById(@PathVariable Long id) {
+		Despesa obj = despesaService.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}	
 	
 	@GetMapping
@@ -54,5 +56,6 @@ public class DespesaResource {
 		List<DespesaDTO> listDTO = list.stream().map(obj -> new DespesaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}	
+	
 	
 }
--- src/main/java/com/example/algamoney/api/service/DespesaService.java
+++ src/main/java/com/example/algamoney/api/service/DespesaService.java
@@ -1,13 +1,14 @@
 package com.example.algamoney.api.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.model.Despesa;
 import com.example.algamoney.api.repository.DespesaRepository;
+import com.example.algamoney.exception.ObjectNotFoundException;
 
 @Service
 public class DespesaService {
@@ -22,4 +23,10 @@ public class DespesaService {
 	public List<Despesa> findAll() {
 		return despesaRepository.findAll();
 	}
+	
+	public Despesa findById(Long id) {
+		Optional<Despesa> obj = despesaRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: "+id + "Tipo: " + Despesa.class.getName()));
+	}	
 }
